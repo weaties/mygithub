18a250376db047cdef595cae3801a2c81cef1f6b
Dan Weatbrook <weaties+github@gmail.com>
2012-01-02 19:33:07 -0800

    adding some changes for shits and grins - don't really know what I am doing here.

--- js/helloworld.js
+++ js/helloworld.js
@@ -2,3 +2,9 @@ var foo=5;
 var bar=5;
 var d=Date();
 console.log('hello world' + d);
+
+var parents = ["Jani", "Tove"];
+var children = ["Cecilie", "Lone"];
+var family = parents.concat(children);
+console.log(family);
+console.log(navigator.appCodeName);
